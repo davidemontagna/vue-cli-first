d470a0380b8b5b2868615e1a86e04d06f1d8ccaf
davidemontagna <davidemontagna92@gmail.com>
2022-01-26 16:31:21 +0100

    done

---
+++ src/components/Hero.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="jumbotron">
+      <img src="../assets/hello-world.jpg">
+      <h2>Non so se questo possa essere definito Jumbotron</h2>
+      <hr>
+      <p>Ma ho provato comunque ad inserire un secondo componente.</p>
+      
+    </div>    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h2{
+  font-size: 30px;
+}
+p{
+  font-size: 20px;
+}
+.jumbotron{
+  padding: 10px;
+  background-color: rgb(207, 231, 231);
+}
+.jumbotron img{
+  height: 300px;
+}
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Title',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h1 {
+  font-size: 40px;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
